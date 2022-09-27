cd8b3aa654363d99bde13d418ebd7fc7d16eca9f
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2022-09-27 14:36:59 +0200

    add automatic dayes searcher

---
+++ adventofcode/Days/Day4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode.Days
+{
+    public class Day4 : IExecute
+    {
+        public void Execute()
+        {
+            
+        }
+    }
+}
--- adventofcode/Program.cs
+++ adventofcode/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using adventofcode;
+using System.Reflection;
+
 Console.WriteLine("https://adventofcode.com/");
+
+var typeToExecute =
+    Assembly.GetAssembly(typeof(Program))?
+    .GetTypes()
+    .Where(s => typeof(IExecute).IsAssignableFrom(s) && s != typeof(IExecute))
+    .OrderBy(s => s.Name)
+    .FirstOrDefault();
+
+if (typeToExecute == null)
+{
+    return;
+}
+
+var executeType = (IExecute)Activator.CreateInstance(typeToExecute);
+
+Console.WriteLine(typeToExecute.Name);
+
+executeType.Execute();
+
+
+
+
+
+
+
+
